394dd991e449ccd9bd08d01138b2ad220641643b
unknown <shaikh.ayan737@gmail.com>
2021-08-09 00:20:07 +0530

    logical programs

---
+++ src/day_6_programs/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package day_6_programs;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		int b=1;
+		System.out.print(a+" "+b);
+		int c;
+		for(int i=1;i<=10;i++) {
+			c=a+b;
+			System.out.print(" "+c);
+			a=b;
+			b=c;
+			
+		}
+
+	}
+
+}
